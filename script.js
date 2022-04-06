31be4a7dab8a2451e3ef271d01842adf8dbb643b
Farxod <farxodjon77000@gmail.com>
2022-04-06 06:49:19 +0500

    meni uy ishim

---
+++ script.js
@@ -0,0 +1,19 @@
+
+let ism = prompt('ismingizni kiriting ')
+let yosh = prompt(' yoshingizni kiriting')
+let misol1 = prompt("misolni yeching 10 + 8 = ?")
+let misol2 = prompt("misolni yeching 18 - 9 = ?")
+let misol3 = prompt("misolni yeching 10 - 9 = ?")
+let misol4 = prompt("misolni yeching 20 - 9 = ?")
+let misol5 = prompt("misolni yeching 10 * (4 + 5) = ?")
+let misol6 = prompt("misolni yeching 10 * (489 + 562) + (370 - 120) = ?")
+alert ("Natijani bilish uchun console ni oching")
+
+console.log("sizning ismim "+ism);
+console.log("sizning mening yoshim "+yosh);
+console.log("misol: 10 + 8 = 18 sizning javob "+misol1);
+console.log("misol: 10 + 8 = 9 sizning javob "+misol2);
+console.log("misol: 10 - 9 = 1 sizning javob "+misol3);
+console.log("misol: 20 - 9 = 11 sizning javob "+misol4);
+console.log("misol: 10 * (4 + 5) = 90 sizning javob "+misol5);
+console.log("misolni yeching 10 * (489 + 562) + (370 - 120) = 10760 sizning javob "+misol5);
